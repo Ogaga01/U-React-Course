c7dc5d6d67f6a64bf9f84cb42112aa2f3e1c7a16
Ogaga Iyara <iyaraogaga69@gmail.com>
2023-07-20 13:21:40 +0100

    Wait first

--- 16-fast-react-pizza/Vite-Pizza/vite-project/src/App.jsx
+++ 16-fast-react-pizza/Vite-Pizza/vite-project/src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./UI/Home";
+import Menu, {loader as menuLoader} from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+import AppLayout from "./UI/AppLayout";
 
-function App() {
-  const [count, setCount] = useState(0)
+const router = createBrowserRouter([
+  {
+    element: <AppLayout/>,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+      },
+      { path: "/order/:orderId", element: <Order /> },
+    ]
+  }
+]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
-export default App
+export default App;
---
+++ 16-fast-react-pizza/Vite-Pizza/vite-project/src/features/menu/Menu.jsx
@@ -0,0 +1,18 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
+
+const Menu = () => {
+  const menu = useLoaderData()
+  console.log(menu)
+  return <ul>{menu.map((pizza)=>{
+    return <MenuItem pizza={pizza} key={pizza.id} />
+  })}</ul>;
+};
+
+export const loader = async ()=>{
+  const menu = await getMenu()
+  return menu
+}
+
+export default Menu;
